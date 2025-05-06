32638885488f38d97b3f1452e5e524bc10c10da9
Marialafu <lafuentelhmaria@gmail.com>
2025-05-06 16:52:13 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ const App = () => {
   return (
     <>
       <CardGroup>
-        <CardComponent>Sedans</CardComponent>
-        <CardComponent>Suvs</CardComponent>
-        <CardComponent>Luxury</CardComponent>
+        <CardComponent title='Sedans' text='Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.' primaryColorText='orange-text' primaryColor='orange' img='./public/assets/images/sedans.svg'></CardComponent>
+        <CardComponent title='Suvs' text='Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.' primaryColorText='blue-text' primaryColor='blue' img='./public/assets/images/suvs.svg'></CardComponent>
+        <CardComponent title='Luxury' text='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.' primaryColorText='green-text' primaryColor='green' img='./public/assets/images/luxury.svg'></CardComponent>
       </CardGroup>
     </>
   );
--- src/components/card-component/CardComponent.jsx
+++ src/components/card-component/CardComponent.jsx
@@ -1,9 +1,16 @@
-const CardComponent = () => {
+import styles from './card-component.module.css'
+
+const CardComponent = ({primaryColor, primaryColorText, text, title, img}) => {
+
+  const cardComponentClass = `card-component ${styles[primaryColor]}`
+  const buttonsClass = `buttons ${styles[primaryColorText]}`
+
   return (
-    <div className='card-component'>
-      <h2></h2>
-      <p></p>
-      <button></button>
+    <div className={cardComponentClass}>
+      <img src={img} alt="" />
+      <h2 className='title'>{title}</h2>
+      <p className='text'>{text}</p>
+      <button className={buttonsClass}>Learn More</button>
     </div>
   );
 };
